c24992ebbdd331438200afb7b185c7a379d61d95
Borchani yessine <borchaniz96@gmail.com>
2019-11-26 15:58:30 +0100

    almost done, just rechecking the code

--- src/main/java/PDFDocProcessor.java
+++ src/main/java/PDFDocProcessor.java
@@ -1,7 +1,10 @@
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.TextChunk;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.property.TextAlignment;
 
 import java.io.FileNotFoundException;
 import java.util.regex.Pattern;
@@ -9,7 +12,11 @@ import java.util.regex.Pattern;
 public class PDFDocProcessor {
     private Document document;
     private Paragraph currentParagraph = new Paragraph();
-
+    private int size = 0;
+    private boolean italic = false;
+    private boolean bold = false;
+    private float indent = 0;
+    private boolean fill = false;
 
     PDFDocProcessor(String destination) throws FileNotFoundException {
         PdfWriter writer = new PdfWriter(destination);
@@ -17,31 +24,63 @@ public class PDFDocProcessor {
         document = new Document(pdfDoc);
     }
 
-    void processLine(String line){
-        if (line.equals(".paragraph") && !currentParagraph.isEmpty()){
-            document.add(currentParagraph);
-            currentParagraph = new Paragraph();
-        }else if (line.equals(".fill")){
-
-        }else if(line.equals(".nofill")){
-
-        }else if (line.equals(".regular")){
-
-        }else if (line.equals(".italic")){
-
-        }else if(line.equals(".bold")){
-
-        }else if (Pattern.matches("^\\.indent \\+[0-9]*",line)){
-
-        }else if (Pattern.matches("^\\.indent \\-[0-9]*",line)){
-
-        }else{
-
+    public void processLine(String line) {
+        if (line.equals(".paragraph") && !currentParagraph.isEmpty()) {
+            startNewParagraph();
+        } else if (line.equals(".fill")) {
+            startNewParagraph();
+            fill = true;
+        } else if (line.equals(".nofill")) {
+            startNewParagraph();
+            fill = false;
+        } else if (line.equals(".regular")) {
+            italic = false;
+            bold = false;
+        } else if (line.equals(".italic")) {
+            italic = true;
+        } else if (line.equals(".bold")) {
+            bold = true;
+        } else if (line.equals(".large")) {
+            size = 2;
+        } else if (line.equals(".small")) {
+            size = 0;
+        } else if (line.equals(".normal")) {
+            size = 1;
+        } else if (Pattern.matches("^\\.indent [\\+\\-][0-9]*", line)) {
+            startNewParagraph();
+            indent += 20f * Float.parseFloat(line.substring(8));
+            currentParagraph.setMarginLeft(indent);
+        } else {
+            Text text = new Text((currentParagraph.isEmpty() ? "" : " ") + line);
+            if (bold) text.setBold();
+            if (italic) text.setItalic();
+            switch (size) {
+                case 0:
+                    text.setFontSize(8f);
+                    break;
+                case 1:
+                    text.setFontSize(11f);
+                    break;
+                case 2:
+                    text.setFontSize(16f);
+                    break;
+            }
+            currentParagraph.add(text);
         }
-
     }
 
-    public void finalize(){
+    public void finalize() {
+        if (!currentParagraph.isEmpty())
+            document.add(currentParagraph);
         document.close();
+    }
+
+    private void startNewParagraph() {
+        if (!currentParagraph.isEmpty()){
+            currentParagraph.setMarginLeft(indent);
+            if (fill) currentParagraph.setTextAlignment(TextAlignment.JUSTIFIED);
+            document.add(currentParagraph);
+        }
+        currentParagraph = new Paragraph();
     }
 }
